28f27d958a977ca73b33ef7d8813428596db1a2e
TTaylor <ttaylor@cnsonline.net>
2024-08-06 14:31:57 +0100

    Extract North String to Constant

--- src/main/java/MarsRover.java
+++ src/main/java/MarsRover.java
@@ -1,10 +1,12 @@
 public class MarsRover {
 
+    public static final String NORTH = "North";
     private String direction;
 
     public MarsRover(String direction) {
         this.direction = direction;
     }
+
     public String getDirection() {
         return direction;
     }
@@ -12,14 +14,13 @@ public class MarsRover {
     public void turn(String instruction) {
         if (instruction.equals("L")) {
             switch (direction) {
-                case "North" -> direction = "West";
+                case NORTH -> direction = "West";
                 case "South" -> direction = "East";
                 case "West" -> direction = "South";
-                case "East" -> direction = "North";
+                case "East" -> direction = NORTH;
             }
-        }
-        else if (instruction.equals("R")) {
-            if (direction.equals("North")) {
+        } else if (instruction.equals("R")) {
+            if (direction.equals(NORTH)) {
                 direction = "East";
             }
         }
