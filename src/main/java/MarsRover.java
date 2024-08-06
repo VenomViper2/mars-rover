8751207b19b8d93d182bbde3244c3eeab5e9e7c7
TTaylor <ttaylor@cnsonline.net>
2024-08-06 12:40:58 +0100

    Tests added for turning Left

--- src/main/java/MarsRover.java
+++ src/main/java/MarsRover.java
@@ -1,5 +1,20 @@
 public class MarsRover {
+
+    private String direction;
+
+    public MarsRover(String direction) {
+        this.direction = direction;
+    }
     public String getDirection() {
-        return "L";
+        return direction;
+    }
+
+    public void turn(String instruction) {
+        switch (direction) {
+            case "North" -> direction = "West";
+            case "South" -> direction = "East";
+            case "West" -> direction = "South";
+            case "East" -> direction = "North";
+        }
     }
 }
--- src/test/java/MarsRoverTest.java
+++ src/test/java/MarsRoverTest.java
@@ -8,9 +8,32 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class MarsRoverTest {
 
     @Test
-    void marsRoverCanTurnLeft() {
-        MarsRover marsRover = new MarsRover();
-        assertEquals("L", marsRover.getDirection());
+    void marsRoverFacesWestWhenTurningLeftFromNorth() {
+        MarsRover marsRover = new MarsRover("North");
+        marsRover.turn("L");
+        assertEquals("West", marsRover.getDirection());
     }
+
+    @Test
+    void marsRoverFacesSouthWhenTurningLeftFromWest() {
+        MarsRover marsRover = new MarsRover("West");
+        marsRover.turn("L");
+        assertEquals("South", marsRover.getDirection());
+    }
+
+    @Test
+    void marsRoverFacesEastWhenTurningLeftFromSouth(){
+        MarsRover marsRover=new MarsRover("South");
+        marsRover.turn("L");
+        assertEquals("East",marsRover.getDirection());
+    }
+
+    @Test
+    void marsRoverFacesNorthWhenTurningFromEast() {
+        MarsRover marsRover = new MarsRover("East");
+        marsRover.turn("L");
+        assertEquals("North", marsRover.getDirection());
+    }
+
 
 }
